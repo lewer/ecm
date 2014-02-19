a6b6a5fa23b4251e52576f9738abbbb92d719aa2
Gabriel Lewertowski <glewerto@gmail.com>
2014-02-19 14:03:02 +0100

    Mise en un seul fichier

--- arith.py
+++ arith.py
@@ -5,6 +5,71 @@
 # A range of number-theoretic algorithms.
 # Compatible with the doctest module.
 
+import random
+import fractions
+
+def AMR(n, k = 3) :
+    if n % 2 == 0 :
+        return False
+    
+    d, s = n-1, 0
+    while (d % 2 == 0):
+        d, s = d/2, s+1
+
+    def WitnessLoop():
+        a = random.randint(2, n-2)
+        x = pow(a, d, n)
+        if (x == 1 or x == n - 1):
+            return True
+        for j in range(s-1):
+            x = pow(x, 2, n)
+            if x == 1:
+                return False
+            if x == n-1:
+                return True
+        return False
+
+    for i in range(k):
+        if not WitnessLoop():
+            return False
+    
+    return True #n est probablement premier
+
+def Pollard_rho(N,f = lambda x, m : (x*x +1) % m):
+    """
+    Computes a non-trivial factor of the integer N
+    (presumed composite) using the Pollard rho algorithm.
+    The second argument, f, is a pseudo-random function modulo N.
+    """
+    x, y, g = 1, 1 ,1
+    while (g == 1):
+        x, y = f(x, N), f(f(y, N), N)
+        g = fractions.gcd(x - y, N)
+    
+    if (g == N):
+        # if the algorithm fails:
+        raise Exception("could not compute factor")
+    else:
+        return g
+
+def FactorPollard(N, k=3):
+    try:
+        d = Pollard_rho(N)
+    except:
+        return [N]
+
+    facteurs = []
+    if AMR(d, k):
+        facteurs += [d]
+    else:
+        facteurs += FactorPollard(d)
+
+    if AMR(N/d, k):
+        facteurs += [N/d]
+    else:
+        facteurs += FactorPollard(N/d)
+        
+    return facteurs
 
 def primes_up_to(bound):
     """
