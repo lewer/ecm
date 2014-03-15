9a0e485655fdbf82170c738c102d1fe857685a19
Gabriel Lewertowski <glewerto@gmail.com>
2014-03-15 17:53:51 +0100

    implémente phase 2 de p-1

--- pmoinsun.py
+++ pmoinsun.py
@@ -1,34 +1,55 @@
 # -*- coding: utf-8 -*-
 import utils
+import math
+import fractions
 
 
-def un_facteur(n):
+def un_facteur(n, b1=10**6, phase2=True, b2=10**8):
     """
     Renvoie un facteur de n avec la méthode p-1
 
     """
 
     b = 2
-    bound = 10**7
-    primes_up_to_bound = utils.compute_prime_numbers_up_to(bound)
+    primes_up_to_bound = utils.big_eratho(b1)
     for p in primes_up_to_bound:
-        q = utils.valuation(bound, p)
+        q = utils.valuation(b1, p)
         b = pow(b, q, n)
-        g = utils.XGCD(b-1, n)[0]
+        g = fractions.gcd(b-1, n)
         if g > 1:
             break
 
     if g == 1:
-        print 'g=1'
-        raise Exception('Aucun facteur calculé : B1 trop petit')
+        if not phase2:
+            raise Exception('Aucun facteur calculé : il faut augmenter B1 ou faire la phase 2')
+
+        # Phase 2, continuation standard
+        # On tabule b^i pour i <= log(b2)^2
+        c = [pow(b, i, n) for i in range(1, int(math.log(b2)**2) + 1)]
+
+        primes = utils.big_segmented_eratho(b1, b2)  # nombres premiers entre b1 et b2
+        s1 = primes.next()
+        t = pow(b, s1, n)  # ta va itérativement contenir b^s pour tous les s premiers, b1 <= s <= b2
+        for i, s2 in enumerate(primes):
+            print "iteration " + str(i)
+            pgcd = fractions.gcd(t, n)
+            if pgcd > 1:
+                return pgcd
+
+            t = (t*c[s2 - s1]) % n  # s1, s2 = primes[i], primes[i+1]
+            s1 = s2
+
+        pgcd = fractions.gcd(t, n)
+        if pgcd == 1:
+            raise Exception('Aucun facteur trouvé après la phase 2')
+
     elif g == n:
-        print 'g=n'
-        raise Exception('Aucun facteur calculé : B1 trop grand')
+        raise Exception('Aucun facteur calculé : Il faut réduire b1')
     else:
         return g
 
 
-def factorise(N, k=3):
+def factorise(N, k=3, b1=10**6):
     """
     Factorise un entier avec la méthode un_facteur
 
@@ -42,7 +63,7 @@ def factorise(N, k=3):
     """
 
     try:
-        d = un_facteur(N)
+        d = un_facteur(N, b1)
     except:
         return [N]
 
